41494c0b8792c6e8c2b7147208e39da30c135935
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2025-03-13 16:49:01 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,26 @@
+import pygame as pg
+
+def game():
+    pg.init()
+
+    screen = pg.display.set_mode((1024, 900))
+    screen.fill((255, 255, 255))
+
+    is_game = True
+
+    while is_game:
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                is_game = False
+                break
+                    
+            elif (event.type == pg.MOUSEBUTTONDOWN) and (pg.mouse.get_pressed(num_buttons=3)[0]):
+                x, y = pg.mouse.get_pos()
+                print(x, y)
+
+        pg.display.flip()
+
+game()
+
+print("end")
