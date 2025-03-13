f854f1cb9e0a7850eee52298a3b6340db7e341c4
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2025-03-13 17:52:00 +0300

    Update game.py

--- game.py
+++ game.py
@@ -7,8 +7,9 @@ def game():
     settings_screen = pg.display.set_mode((500, 400))
     settings_screen.fill((255, 255, 255))
     pg.display.set_caption("Minesweeper with pygame")
-    
+
     is_game = True
+    difficulty = ""
 
     while is_game:
         settings_screen.blit(settings_menu, (0, 0))
@@ -21,6 +22,7 @@ def game():
             elif (event.type == pg.MOUSEBUTTONDOWN) and (pg.mouse.get_pressed(num_buttons=3)[0]):
                 x, y = pg.mouse.get_pos()
                 print(x, y)
+                settings_screen.blit(selecting_arrow, (x, y))
 
         pg.display.flip()
 
