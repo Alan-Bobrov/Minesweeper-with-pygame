0530a99181996b66ae58af2ade1cac5f85ead256
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2025-03-13 18:40:25 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,45 +1,52 @@
 import pygame as pg
 from images.images import *
 
-def game():
+def SettingsMenu():
     pg.init()
 
     settings_screen = pg.display.set_mode((500, 400))
     settings_screen.fill((255, 255, 255))
     pg.display.set_caption("Minesweeper with pygame")
 
-    is_game = True
     difficulty = ""
 
-    while is_game:
+    while True:
         settings_screen.blit(settings_menu, (0, 0))
         if difficulty:
             if difficulty == "Beginner":
                 settings_screen.blit(selecting_arrow, (8, 192))
             elif difficulty == "Intermediate":
-                settings_screen.blit(selecting_arrow, (8, 260))
+                settings_screen.blit(selecting_arrow, (8, 252))
             elif difficulty == "Expert":
-                settings_screen.blit(selecting_arrow, (8, 320))
+                settings_screen.blit(selecting_arrow, (8, 312))
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                is_game = False
-                break
+                return 0
 
-            elif (event.type == pg.MOUSEBUTTONDOWN) and (pg.mouse.get_pressed(num_buttons=3)[0]):
+            elif (event.type == pg.MOUSEBUTTONDOWN):
                 x, y = pg.mouse.get_pos()
                 print(x, y)
 
-                if (0 < x < 330) and (185 < y < 250):
-                    print(1)
+                if (0 < x < 415) and (185 < y < 250):
+                    if difficulty == "Beginner":
+                        print(1)
+                        return 1
                     difficulty = "Beginner"
-                elif () and ():
-                    pass
-                elif () and ():
-                    pass
+                elif (0 < x < 415) and (251 < y < 300):
+                    if difficulty == "Intermediate":
+                        print(2)
+                        return 2
+                    difficulty = "Intermediate"
+                elif (0 < x < 415) and (300 < y):
+                    if difficulty == "Expert":
+                        print(3)
+                        return 3
+                    difficulty = "Expert"
 
         pg.display.flip()
 
-game()
+a = SettingsMenu()
+print(a)
 
 print("end")
